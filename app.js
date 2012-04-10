800bde633c83d4756bcfa4fc4adc637e02931a8a
Roger Castillo <rcastillo@Rogers-MacBook-Pro.local>
2012-04-09 22:50:10 -0500

    initial check-in of code

---
+++ app.js
@@ -0,0 +1,46 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+var bundle = require('browserify')({
+  entry:__dirname + '/lib/client/index.js',
+  watch:true
+});
+
+
+// Server Configuration
+app.configure(function(){
+
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+
+  //static files
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/public/js'));
+
+  app.use(bundle);
+
+  // Session support
+  app.use(express.cookieParser());
+
+  var RedisStore = require('connect-redis')(express);
+
+  store = new RedisStore();
+
+  app.use(express.session({
+    secret: "Rush 2112",
+    store: store
+  }));
+
+});
+
+// Server Routes
+
+app.listen(3000);
+
+console.log("Server ready: Throw 'em up!!!");
+
+
